aa1cf56101d12318aa37bf1e2b286919b6d666fe
jackhaha363 <fdsafdsa@dsafdsa.com>
2019-12-29 20:24:10 -0500

    Use virtual env

--- scripts/run_mila.sh
+++ scripts/run_mila.sh
@@ -2,6 +2,7 @@
 
 export PYTHONUNBUFFERED=1
 
-source /network/home/luyuchen/miniconda2/etc/profile.d/conda.sh
-conda activate py36
+#source /network/home/luyuchen/miniconda2/etc/profile.d/conda.sh
+#conda activate py36
+source /network/home/luyuchen/py36/bin/activate
 exec $@
