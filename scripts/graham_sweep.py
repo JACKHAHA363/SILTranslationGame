34353eb8b16957dfce9143619e9c1e5e80180d03
jackhaha363 <fdsafdsa@dsafdsa.com>
2020-01-12 15:20:18 -0500

    finalize sweep script

--- scripts/graham_sweep.py
+++ scripts/graham_sweep.py
@@ -7,6 +7,7 @@ from itertools import product
 import itlearn
 import os
 import sys
+from subprocess import call
 
 PROJ_PATH = os.path.dirname(itlearn.__file__)
 SCRIPT_PATH = os.path.join(PROJ_PATH, 'train.py')
@@ -15,6 +16,7 @@ SLURM_FILE = os.path.join(os.path.dirname(PROJ_PATH), 'scripts', 'run_graham.sh'
 print('train_script', SCRIPT_PATH)
 print('python bin', PYBIN)
 print('slurm run file', SLURM_FILE)
+EXCLUDES = ['save_at']
 
 SLURM_OPTION = ['sbatch', SLURM_FILE]
 
@@ -24,6 +26,7 @@ def get_args():
     parser.add_argument('--data_dir', required=True)
     parser.add_argument('--exp_dir', required=True)
     parser.add_argument('--sweep', required=True, help='path to json of sweep')
+    parser.add_argument('--test', action='store_true', help='not calling just printing')
     return parser.parse_args()
 
 
@@ -32,24 +35,25 @@ def main():
     with open(args.sweep, 'r') as f:
         sweep_json = json.load(f)
     all_keys = list(sweep_json.keys())
-    sweep_keys = [k for k in all_keys if isinstance(sweep_json[k], list)]
+    sweep_keys = [k for k in all_keys if isinstance(sweep_json[k], list) and k not in EXCLUDES]
     non_sweep_keys = [k for k in all_keys if k not in sweep_keys]
     print('Sweeping ', sweep_keys)
 
     # Command with fixed args
     cmd = [PYBIN, SCRIPT_PATH,
-           '--data_dir', args.data_dir,
-           '--exp_dir', args.exp_dir,
-           '--config', args.sweep]
+           '--data_dir', os.path.abspath(args.data_dir),
+           '--exp_dir', os.path.abspath(args.exp_dir),
+           '--config', os.path.abspath(args.sweep)]
     for k in non_sweep_keys:
         cmd += ['--{}'.format(k), str(sweep_json[k])]
-
     for sweep_args in product(*[sweep_json[k] for k in sweep_keys]):
         non_fixed = []
         for k, val in zip(sweep_keys, sweep_args):
             non_fixed += ['--{}'.format(k), str(val)]
         final_cmd = SLURM_OPTION + cmd + non_fixed
         print(final_cmd)
+        if not args.test:
+            call(final_cmd)
 
 
 if __name__ == '__main__':
