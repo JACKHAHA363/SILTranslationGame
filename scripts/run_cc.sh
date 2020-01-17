09cccdd3ee7e32f601f9136c10465eaace8af24c
jackhaha363 <fdsafdsa@dsafdsa.com>
2020-01-17 18:34:01 -0500

    cc becomes 6 hours

--- scripts/run_cc.sh
+++ scripts/run_cc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:p100:1
 #SBATCH --mem=16G
-#SBATCH --time=4:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mail-user=luyuchen.paul@gmail.com
 #SBATCH --mail-type=END,FAIL
 
