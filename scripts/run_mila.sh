ce98b5882ed1b5af675897d60372cf218f885890
jackhaha363 <fdsafdsa@dsafdsa.com>
2020-04-13 00:09:52 -0400

    show node info on slurm

--- scripts/run_mila.sh
+++ scripts/run_mila.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 #SBATCH --gres=gpu:1
 #SBATCH --mem=8G
-#SBATCH --exclude=eos21,bart14
+#SBATCH --exclude=eos21,bart14,leto32
 #SBATCH --mail-user=luyuchen.paul@gmail.com
 #SBATCH --mail-type=END,FAIL
 
+echo "running on $SLURMD_NODENAME"
 export PYTHONUNBUFFERED=1
 
 #source /network/home/luyuchen/miniconda2/etc/profile.d/conda.sh
