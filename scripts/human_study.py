fa9a76da8ceec4d391c379820e04727c17c78c57
jackhaha363 <fdsafdsa@dsafdsa.com>
2020-04-14 20:31:56 -0400

    generate human study done

--- scripts/human_study.py
+++ scripts/human_study.py
@@ -1,8 +1,6 @@
 import argparse
 import os
-import string
 import random
-from itertools import product
 import csv
 import pickle
 
@@ -10,8 +8,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-txt_dir', required=True)
 parser.add_argument('-fr', required=True)
 parser.add_argument('-outdir', required=True)
-parser.add_argument('-nb_examples', default=50)
-parser.add_argument('-nb_csv', default=1)
+parser.add_argument('-nb_examples', default=25)
+parser.add_argument('-nb_csv', default=20)
 args = parser.parse_args()
 
 if not os.path.exists(args.outdir):
@@ -20,11 +18,11 @@ if not os.path.exists(args.outdir):
 
 # Read Eng
 files = sorted(os.listdir(args.txt_dir))
-data = {name: [line.rstrip('\n').replace('&apos;', "'").replace('&quot;', '"')
+hyps = {name: [line.rstrip('\n').replace('&apos;', "'").replace('&quot;', '"')
 			   for line in open(os.path.join(args.txt_dir, name))] for name in files}
 for name in files:
-	print(name, 'has {} line'.format(len(data[name])))
-	for line in data[name]:
+	print(name, 'has {} line'.format(len(hyps[name])))
+	for line in hyps[name]:
 		assert "&" not in line, line
 
 # Read French
@@ -33,54 +31,43 @@ for line in frs:
 	assert '&' not in line, line
 
 
-def randomString(stringLength=10):
-	""" Generate a random string of fixed length """
-	letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
-	return ''.join(random.choice(letters) for i in range(stringLength))
-
-
-while True:
-	sent_ids = [randomString() for _ in range(len(files) * len(frs))]
-	if len(sent_ids) == len(set(sent_ids)):
-		print('Sentence ids generated!')
-		break
-
-info2id, id2info = {}, {}
-for idx, info in enumerate(product(files, range(len(frs)))):
-	sent_id = sent_ids[idx]
-	info2id[info] = sent_id
-	id2info[sent_id] = info
-
-pickle.dump({'info2id': info2id,
-			 'id2info': id2info}, open(os.path.join(args.out_dir, 'data.pkl')))
-
 ex_ids = [i for i in range(len(frs))]
+datas = []
 for person_id in range(args.nb_csv):
+	data = {'w_src': [], 'wo_src': []}
+
 	# Pick unique ex id for both experiments
 	random.shuffle(ex_ids)
 	exp2_ex_ids = ex_ids[-args.nb_examples:].copy()
 
 	# Exp1
-	with open(os.path.join(args.outdir, 'exp1_{}.csv'.format(person_id)), 'w', newline='') as csvfile:
+	with open(os.path.join(args.outdir, 'w_src_{}.csv'.format(person_id)), 'w', newline='') as csvfile:
 		writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
-		writer.writerow(["Model", "ID", "Sentence"])
 		for test_id, ex_id in enumerate(ex_ids[:args.nb_examples]):
 			fr = frs[ex_id]
-			writer.writerow(["Test{}".format(test_id), "", fr])
+			writer.writerow(["Test{}".format(test_id), fr])
 			random.shuffle(files)
 			methodA, methodB = files[:2]
-			writer.writerow(["A", info2id[(methodA, ex_id)], data[methodA][ex_id]])
-			writer.writerow(["B", info2id[(methodB, ex_id)], data[methodB][ex_id]])
-			writer.writerow(["result", "?", ""])
+			writer.writerow(["A", hyps[methodA][ex_id]])
+			writer.writerow(["B", hyps[methodB][ex_id]])
+			writer.writerow(["Which is Better: ('A', 'B', or '-')", ""])
+			writer.writerow(["", ""])
+			data['w_src'].append((ex_id, methodA, methodB))
 
-	# Exp1
-	with open(os.path.join(args.outdir, 'exp2_{}.csv'.format(person_id)), 'w', newline='') as csvfile:
+	# Exp2
+	with open(os.path.join(args.outdir, 'wo_src_{}.csv'.format(person_id)), 'w', newline='') as csvfile:
 		writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
-		writer.writerow(["Model", "ID", "Sentence"])
 		for test_id, ex_id in enumerate(ex_ids[-args.nb_examples:]):
-			writer.writerow(["Test{}".format(test_id), "", ""])
+			writer.writerow(["Test{}".format(test_id), ""])
 			random.shuffle(files)
 			methodA, methodB = files[:2]
-			writer.writerow(["A", info2id[(methodA, ex_id)], data[methodA][ex_id]])
-			writer.writerow(["B", info2id[(methodB, ex_id)], data[methodB][ex_id]])
-			writer.writerow(["result", "?", ""])
+			writer.writerow(["A", hyps[methodA][ex_id]])
+			writer.writerow(["B", hyps[methodB][ex_id]])
+			writer.writerow(["Which is Better: ('A', 'B', or '-')", ""])
+			writer.writerow(["", ""])
+			data['wo_src'].append((ex_id, methodA, methodB))
+
+	datas.append(data)
+
+with open(os.path.join(args.outdir, 'data.pkl'), 'wb') as f:
+	pickle.dump(datas, f)
