cb444ef6d779b8b71236b57b12d7834d8ec24e28
jackhaha363 <fdsafdsa@dsafdsa.com>
2020-04-16 16:34:55 -0400

    re-introduce sentence ID for robustness.

--- scripts/human_study/generate.py
+++ scripts/human_study/generate.py
@@ -2,14 +2,16 @@ import argparse
 import os
 import random
 import csv
+import string
+from itertools import product
 import pickle
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-txt_dir', required=True)
 parser.add_argument('-fr', required=True)
 parser.add_argument('-outdir', required=True)
-parser.add_argument('-nb_examples', default=25)
-parser.add_argument('-nb_csv', default=20)
+parser.add_argument('-nb_examples', default=25, type=int)
+parser.add_argument('-nb_csv', default=20, type=int)
 args = parser.parse_args()
 
 if not os.path.exists(args.outdir):
@@ -31,11 +33,30 @@ for line in frs:
 	assert '&' not in line, line
 
 
-ex_ids = [i for i in range(len(frs))]
-datas = []
-for person_id in range(args.nb_csv):
-	data = {'w_src': [], 'wo_src': []}
+def randomString(stringLength=10):
+	""" Generate a random string of fixed length """
+	letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+	return ''.join(random.choice(letters) for i in range(stringLength))
 
+
+while True:
+	sent_ids = [randomString() for _ in range(len(files) * len(frs))]
+	if len(sent_ids) == len(set(sent_ids)):
+		print('Sentence ids generated!')
+		break
+
+info2id, id2info = {}, {}
+for idx, info in enumerate(product(files, range(len(frs)))):
+	sent_id = sent_ids[idx]
+	info2id[info] = sent_id
+	id2info[sent_id] = info
+
+pickle.dump({'info2id': info2id,
+			 'id2info': id2info}, open(os.path.join(args.outdir, 'id_data.pkl'), 'wb'))
+
+
+ex_ids = [i for i in range(len(frs))]
+for person_id in range(args.nb_csv):
 	# Pick unique ex id for both experiments
 	random.shuffle(ex_ids)
 	exp2_ex_ids = ex_ids[-args.nb_examples:].copy()
@@ -45,14 +66,13 @@ for person_id in range(args.nb_csv):
 		writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
 		for test_id, ex_id in enumerate(ex_ids[:args.nb_examples]):
 			fr = frs[ex_id]
-			writer.writerow(["", fr])
+			writer.writerow(["", "", fr])
 			random.shuffle(files)
 			methodA, methodB = files[:2]
-			writer.writerow(["A", hyps[methodA][ex_id]])
-			writer.writerow(["B", hyps[methodB][ex_id]])
-			writer.writerow(["Which is Better: ('A', 'B', or '-')", ""])
-			writer.writerow(["", ""])
-			data['w_src'].append((ex_id, methodA, methodB))
+			writer.writerow([info2id[(methodA, ex_id)], "", hyps[methodA][ex_id]])
+			writer.writerow([info2id[(methodB, ex_id)], "", hyps[methodB][ex_id]])
+			writer.writerow(["", "", ""])
+
 
 	# Exp2
 	with open(os.path.join(args.outdir, 'wo_src_{}.csv'.format(person_id)), 'w', newline='') as csvfile:
@@ -60,13 +80,6 @@ for person_id in range(args.nb_csv):
 		for test_id, ex_id in enumerate(ex_ids[-args.nb_examples:]):
 			random.shuffle(files)
 			methodA, methodB = files[:2]
-			writer.writerow(["A", hyps[methodA][ex_id]])
-			writer.writerow(["B", hyps[methodB][ex_id]])
-			writer.writerow(["Which is Better: ('A', 'B', or '-')", ""])
-			writer.writerow(["", ""])
-			data['wo_src'].append((ex_id, methodA, methodB))
-
-	datas.append(data)
-
-with open(os.path.join(args.outdir, 'data.pkl'), 'wb') as f:
-	pickle.dump(datas, f)
+			writer.writerow([info2id[(methodA, ex_id)], "", hyps[methodA][ex_id]])
+			writer.writerow([info2id[(methodB, ex_id)], "", hyps[methodB][ex_id]])
+			writer.writerow(["", "", ""])
