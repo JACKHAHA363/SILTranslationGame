38dfc7aa8ed31f4d4de61c524a591543121069f1
jackhaha363 <fdsafdsa@dsafdsa.com>
2020-04-27 15:10:45 -0400

    Update CC run script

--- scripts/run_cc.sh
+++ scripts/run_cc.sh
@@ -2,9 +2,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=8G
 #SBATCH --time=20:00:00
-#SBATCH --mail-user=luyuchen.paul@gmail.com
-#SBATCH --mail-type=END,FAIL
-
+#SBATCH --account=def-bengioy
 
 export PYTHONUNBUFFERED=1
 exec $@
