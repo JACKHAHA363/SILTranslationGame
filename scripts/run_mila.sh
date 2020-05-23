3f88df50088844a38d697f11e330bf516be4b343
Yuchen Lu <luyuchen@login-3.server.mila.quebec>
2020-05-23 18:45:25 -0400

    Remove unecessary json and block more nodes

--- scripts/run_mila.sh
+++ scripts/run_mila.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#SBATCH --gres=gpu:titanx:1
-#SBATCH --mem=8G
-#SBATCH --exclude=eos21,bart14,bart13,leto06,leto23,leto32
+#SBATCH --gres=gpu:1
+#SBATCH --mem=6G
+#SBATCH --exclude=eos21,bart14,bart13,leto06,leto23,leto32,leto12,leto11
 #SBATCH --mail-user=luyuchen.paul@gmail.com
 #SBATCH --mail-type=FAIL
 
