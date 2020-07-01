cfa55baeae5701f1082520ab4411862eee6106dc
jackhaha363 <fdsafdsa@dsafdsa.com>
2020-06-30 22:36:42 -0400

    Update analyze

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+# Introduction
+This is the source code for paper 
+
 # Usage
 python train.py --config [JSON] --data_dir [DATA_DIR] --exp_dir [ROOT_EXP]
--- scripts/human_study/analyze.py
+++ scripts/human_study/analyze.py
@@ -9,19 +9,41 @@ import pickle
 import ipdb
 import numpy as np
 from pandas import DataFrame
+from scipy.stats import binom_test
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-csv_dir', required=True)
-parser.add_argument('-idmap', required=True)
+parser.add_argument('-data', required=True)
 args = parser.parse_args()
 
-with open(args.idmap, 'rb') as f:
-    idmap = pickle.load(open(args.idmap, 'rb'))
-methods = idmap['methods']
-id2info = idmap['id2info']
+with open(args.data, 'rb') as f:
+    data = pickle.load(open(args.data, 'rb'))
+methods = data['methods']
+id2info = data['id2info']
+hyps = data['hyps']
 csv_files = [f for f in os.listdir(args.csv_dir) if '.csv' in f]
 wo_src = [csv_file for csv_file in csv_files if 'wo_src' in csv_file]
 w_src = [csv_file for csv_file in csv_files if 'w_src' in csv_file]
+
+
+def retrieval_method(sent_id, sent):
+    """ Return the method ID """
+    try:
+        return methods.index(id2info[sent_id][0])
+    except KeyError:
+        pass
+
+    # Try to find by bruteforcing
+    found = []
+    for method in hyps:
+        for target in hyps[method]:
+            if sent == target:
+                found.append(method)
+    found = list(set(found))
+    if len(found) == 1:
+        return methods.index(found[0])
+    else:
+        return None
 
 
 def read_csv(csv_path):
@@ -29,49 +51,47 @@ def read_csv(csv_path):
     state = 'init'
     results = []
     buffer = []
+    nb_exp = 0
     with open(csv_path) as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
-        try:
-            for line in reader:
-                if len(line) != 3:
-                    ipdb.set_trace()
-                sent_id, score, sent = line
-                if state == 'init':
-                    if sent_id == "":
-                        continue
-                    else:
-                        buffer.append((sent_id, score))
-                        state = "one"
-                elif state == "one":
-                    if sent_id == "":
-                        assert False, "Something is wrong at this row {} {} {}".format(sent_id, score, sent)
-                    else:
-                        buffer.append((sent_id, score))
+        for line in reader:
+            if len(line) != 3:
+                ipdb.set_trace()
+            sent_id, score, sent = line
+            if state == 'init':
+                if sent_id == "":
+                    continue
+                else:
+                    method = retrieval_method(sent_id, sent)
+                    if method is not None:
+                        buffer.append((method, score))
+                    state = "one"
+            elif state == "one":
+                if sent_id == "":
+                    assert False, "Something is wrong at this row {} {} {}".format(sent_id, score, sent)
+                else:
+                    method = retrieval_method(sent_id, sent)
+                    if method is not None:
+                       buffer.append((method, score))
+                    if len(buffer) == 2:
                         results.append(buffer)
-                        buffer = []
-                        state = 'init'
-        except ValueError:
-            ipdb.set_trace()
-    print('NB experiments:', len(results))
+                    buffer = []
+                    state = 'init'
+                    nb_exp += 1
+    print('{}:\t{}\t{}%:'.format(csv_path, len(results),
+                               len(results) / float(nb_exp) *100))
     assert len(results) > 0
 
     # Deal with results
     win_mat = np.zeros([len(methods), len(methods)])
     count_mat = np.zeros([len(methods), len(methods)])
     for result in results:
-        id1, score1 = result[0]
-        id2, score2 = result[1]
-        try:
-            method1 = methods.index(id2info[id1][0])
-            method2 = methods.index(id2info[id2][0])
-        except KeyError:
-            if id1 not in id2info:
-                print('{} not found'.format(id1))
-            if id2 not in id2info:
-                print('{} not found'.format(id2))
+        method1, score1 = result[0]
+        method2, score2 = result[1]
+        if score1 == "" and score2 == "":
             continue
-        if score1 == "" or score2 == "":
-            continue
+        score1 = 0 if score1 == "" else int(score1)
+        score2 = 0 if score2 == "" else int(score2)
         if score1 > score2:
             win_mat[method1, method2] += 1
         if score2 > score1:
@@ -94,21 +114,21 @@ def accumulate_csvs(csv_files):
 
 
 def visualize(total_win_mat, total_count_mat):
-    print('Win Ratio')
+    #print('Win Ratio')
     win_ratio = total_win_mat / total_count_mat
-    for i in range(len(methods)):
-        win_ratio[i, i] = 0
-    df = DataFrame(win_ratio, index=methods, columns=methods)
-    print(df)
+    #for i in range(len(methods)):
+    #    win_ratio[i, i] = 0
+    #df = DataFrame(win_ratio, index=methods, columns=methods)
+    #print(df)
 
-    print('Tie Ratio')
-    tie_ratio = np.zeros([len(methods), len(methods)])
-    for i in range(len(methods)):
-        for j in range(len(methods)):
-            tie_ratio[i, j] = 1 - (win_ratio[i, j] + win_ratio[j, i])
-    df = DataFrame(tie_ratio,
-                   index=methods, columns=methods)
-    print(df)
+    #print('Tie Ratio')
+    #tie_ratio = np.zeros([len(methods), len(methods)])
+    #for i in range(len(methods)):
+    #    for j in range(len(methods)):
+    #        tie_ratio[i, j] = 1 - (win_ratio[i, j] + win_ratio[j, i])
+    #df = DataFrame(tie_ratio,
+    #               index=methods, columns=methods)
+    #print(df)
 
     print('Adjusted Win Ratio')
     adjusted_win_ratio = np.zeros([len(methods), len(methods)])
@@ -120,10 +140,31 @@ def visualize(total_win_mat, total_count_mat):
     print(DataFrame(np.concatenate([adjusted_win_ratio, adjusted_win_ratio.sum(-1).reshape([-1, 1])], -1),
                     index=methods, columns=methods + ['SUM']))
 
-    print('Nb. pairs tested:', total_count_mat.sum())
-    df = DataFrame(total_count_mat,
-                   index=methods, columns=methods)
-    print(df)
+    print('Adjusted Count')
+    adjusted_count = np.zeros([len(methods), len(methods)])
+    for i in range(len(methods)):
+        for j in range(len(methods)):
+            if i == j:
+                continue
+            adjusted_count[i, j] = total_count_mat[i, j] * float(win_ratio[i, j] + win_ratio[j, i])
+    print(DataFrame(adjusted_count,
+                    index=methods,
+                    columns=methods))
+    print('Nb. pairs tested:', adjusted_count.sum() / 2)
+
+    print('P-values for single sided binomial test')
+    single_sided_p_value = np.zeros([len(methods), len(methods)])
+    for i in range(len(methods)):
+        for j in range(len(methods)):
+            if i == j:
+                continue
+            n = adjusted_count[i,j]
+            x_win_ratio = adjusted_win_ratio[i,j] if adjusted_win_ratio[i,j] > 0.5 else adjusted_win_ratio[j,i]
+            x = int(n * x_win_ratio)
+            single_sided_p_value[i,j] = binom_test(x, n)
+    print(DataFrame(single_sided_p_value,
+                    index=methods,
+                    columns=methods))
 
 wo_total_win_mat, wo_total_count_mat = accumulate_csvs(wo_src)
 w_total_win_mat, w_total_count_mat = accumulate_csvs(w_src)
